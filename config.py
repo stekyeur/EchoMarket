c085c10b283afa284e9fa2efe3e4664d81db8c86
stekyeur <asyaodulkoc@gmail.com>
2025-11-30 20:51:33 +0300

    Sesli asistan, karşılama ekranı ve supabase veritabanı bağlantısı eklendi

---
+++ config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "aws-1-ap-southeast-2.pooler.supabase.com",
+    "port": "5432",
+    "dbname": "postgres",
+    "user": "postgres.zhulbmvyuszoiutbthpu",
+    "password": "RYca&61au.aMk2//307"
+}
